2529b3b201ead86a602d34b78c1e8ed0b78d8f45
Donald Dominic <gotodonald@gmail.com>
2023-02-03 18:29:56 +0100

    index endpoint

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`server started on :${PORT}`);
