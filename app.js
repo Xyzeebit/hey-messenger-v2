d1872012054661634f3e99f7bb6831310e1864ca
Donald Dominic <gotodonald@gmail.com>
2023-02-03 18:19:54 +0100

    server setup

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(PORT, () => {
     console.log(`server started on :${PORT}`);
