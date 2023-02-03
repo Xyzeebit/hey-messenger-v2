270b5be77208691d294b72471e6cb4e835b5c9a3
Donald Dominic <gotodonald@gmail.com>
2023-02-03 18:48:27 +0100

    setup view

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send({ message: "Hello world" });
+    res.render("index", { name: "Donald" });
 });
 
 module.exports = router;
