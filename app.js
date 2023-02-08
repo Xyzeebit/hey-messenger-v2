9d8c9baa25c39a7024e9340c10f7c8f242ee88e3
Donald Dominic <gotodonald@gmail.com>
2023-02-08 19:49:54 +0100

    added socket connection

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
+const http = require("http");
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 
 const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 const PORT = process.env.PORT;
 
 app.use(express.json());
@@ -12,6 +15,17 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(routes);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+    console.log("connection established");
+
+    socket.on("error", (err) => {
+        console.log(err.message);
+    });
+
+    socket.on('data', console.log);
+    socket.emit('num', Date.now());
+});
+
+server.listen(PORT, () => {
     console.log(`server started on :${PORT}`);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
+'use strict'
+
 window.addEventListener('DOMContentLoaded', function (event) {
+    const socket = io();
+
     const form = document.getElementById("form");
+
     form.addEventListener("submit", (evt) => {
         evt.preventDefault();
-    });  
-})
+    });
+
+
+    socket.on("connection", () => {
+        console.log("connected to server");
+    });
+
+    socket.emit('data', 10);
+
+
+    socket.on('num', console.log);
+
+});
