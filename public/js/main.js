9775666837d0a12e0c135dc5edba011847b84968
Donald Dominic <gotodonald@gmail.com>
2023-02-08 19:34:28 +0100

    added js to index page

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function (event) {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+    });  
+})
