15e450c71a82354d405f1436909313cf1563e2ec
Donald Dominic <gotodonald@gmail.com>
2023-02-17 10:40:20 +0100

    added chat window input button

--- public/js/msg.js
+++ public/js/msg.js
@@ -10,5 +10,37 @@ window.addEventListener('DOMContentLoaded', function (event) {
     socket.on("msg", (msg) => {
         console.log(msg)
     });
+
+
+
+    // component selectors
+    const textarea = document.querySelector('.input__area');
+    const sendBtn = document.querySelector('.btn__send');
+
+    textarea.addEventListener('keyup', handleChatInput);
+    textarea.addEventListener('keyup', enableSendButton);
+
+
+
+
+
+
+
+
+
     
 });
+
+const handleChatInput = (evt) => {
+    evt.target.style.height = "1px";
+    evt.target.style.height = evt.target.scrollHeight + "px";
+}
+
+const enableSendButton = evt => {
+    const sendBtn = document.querySelector(".btn__send");
+    if (evt.target.value.trim()) {
+        sendBtn.removeAttribute('disabled');
+    } else {
+        sendBtn.setAttribute("disabled", true);
+    }
+}
